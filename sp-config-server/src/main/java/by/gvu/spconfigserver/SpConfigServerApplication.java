7397ff46ceeee59514874ccf51b7b51d49fafbbc
Heorhi Utseuski <Heorhi.Utseuski@mckesson.com>
2023-10-31 16:36:48 +0100

    Add module sp-config-server

---
+++ sp-config-server/src/main/java/by/gvu/spconfigserver/SpConfigServerApplication.java
@@ -0,0 +1,13 @@
+package by.gvu.spconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpConfigServerApplication.class, args);
+    }
+
+}
---
+++ sp-config-server/src/test/java/by/gvu/spconfigserver/SpConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gvu.spconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
