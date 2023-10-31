cb7e38c44d3d51b9fe7ddc5c1f97e8bc0d1da09f
Heorhi Utseuski <Heorhi.Utseuski@mckesson.com>
2023-10-31 16:44:40 +0100

    Add module sp-eureka-server

---
+++ sp-eureka-server/src/main/java/by/gvu/speurekaserver/SpEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package by.gvu.speurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpEurekaServerApplication.class, args);
+    }
+
+}
---
+++ sp-eureka-server/src/test/java/by/gvu/speurekaserver/SpEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gvu.speurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
