7ea283c8636ee63fafc29799711b07532b2a5348
Heorhi Utseuski <Heorhi.Utseuski@mckesson.com>
2023-11-01 11:41:31 +0100

    Add module sp-user-service

---
+++ sp-user-service/src/test/java/by/gvu/spuserservice/SpUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.gvu.spuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpUserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
